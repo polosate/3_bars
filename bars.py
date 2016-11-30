f5fab16f145ed0aff48c24e4ac1db50ace5927f5
ashaposhnikova <polosate@gmail.com>
2016-11-30 16:51:21 +0300

    Replaced cp1251 to utf-8, renamed functions

--- bars.py
+++ bars.py
@@ -3,22 +3,22 @@ from math import sqrt
 
 
 def load_data(filepath):
-    with open(filepath, 'r', encoding="cp1251") as data_file:
+    with open(filepath, 'r', encoding="utf-8") as data_file:
         data = json.loads(data_file.read())
     return data
 
 
-def get_biggest_bar(data):
+def get_address_of_biggest_bar(data):
     biggets_bar = max(data, key=lambda x : x['SeatsCount'])
     return biggets_bar['Address']
 
 
-def get_smallest_bar(data):
+def get_address_of_smallest_bar(data):
     smallest_bar = min(data, key=lambda x : x['SeatsCount'])
     return smallest_bar['Address']
 
 
-def get_closest_bar(data, longitude, latitude):
+def get_address_of_closest_bar(data, longitude, latitude):
     closest_bar = min(data, key=lambda x: sqrt(pow(float(x['Longitude_WGS84'])\
         - longitude, 2) + pow(float(x['Latitude_WGS84']) - latitude, 2)))
     return closest_bar['Address']
@@ -26,8 +26,8 @@ def get_closest_bar(data, longitude, latitude):
 if __name__ == '__main__':
     filepath = input('Path to file >> ')
     data = load_data(filepath)
-    print('Biggest bar:', get_biggest_bar(data))
-    print('Smallest bar:', get_smallest_bar(data))
+    print('Address of biggest bar:', get_address_of_biggest_bar(data))
+    print('Address of smallest bar:', get_address_of_smallest_bar(data))
     latitude = float(input('Input latitude >> '))
     longitude = float(input('Input longitude >> '))
-    print('Closest bar:', get_closest_bar(data, longitude, latitude))
+    print('Address of closest bar:', get_address_of_closest_bar(data, longitude, latitude))
